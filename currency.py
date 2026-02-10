4bdb5ca4623e8df8eb199c63c69f2fc14c711d35
abhiraj75 <itsabhiraj27@gmail.com>
2026-02-11 00:57:22 +0530

    handle probable errors

--- currency.py
+++ currency.py
@@ -12,11 +12,24 @@ def convert_currency(base):
         response=requests.get(url)
         data=response.json()
         return data["data"]
-    except Exception as e:
-        print(e)
+    except Exception :
+        print("Currency Not Yet Integrated")
         return None
 
-data=convert_currency("CAD")
-del data["CAD"]
-for ticker,value in data.items():
-    print(f"{ticker}:{value}")
+while True:
+    base=input("Enter The Base Currency (q for quit): ").upper()
+    if base == "Q":
+        break
+
+    data=convert_currency(base)
+    if not data:
+        continue
+    del data[base]
+    amount=input("Enter The Amount You Want To Convert (q for quit):").upper()
+    if amount == "Q":
+        break
+    try:
+        for ticker,value in data.items():
+            print(f"{ticker}:{value*float(amount)}")
+    except Exception:
+        print("Please Enter A Valid Amount!")
