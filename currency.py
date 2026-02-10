1aef3f2e7d2ad92fb4cbc5882447df055e24ada2
abhiraj75 <itsabhiraj27@gmail.com>
2026-02-10 23:55:52 +0530

    complete function

--- currency.py
+++ currency.py
@@ -1,4 +1,20 @@
 import requests
 
-API_KEY= "fca_live_oNSQfRamKggbFdoJvwwbSLCpW2wV8U4goQ0frr3g"
-BASE_URL= f"https://api.freecurrencyapi.com/v1/latest?apikey={API_KEY}"
+API_KEY = "fca_live_oNSQfRamKggbFdoJvwwbSLCpW2wV8U4goQ0frr3g"
+BASE_URL = f"https://api.freecurrencyapi.com/v1/latest?apikey={API_KEY}"
+
+CURRENCIES = ["USD", "CAD", "EUR", "AUD", "CNY"]
+
+def convert_currency(base):
+    currencies= ",".join(CURRENCIES)
+    url=f"{BASE_URL}&base_currency={base}&currencies={currencies}"
+    try:
+        response=requests.get(url)
+        data=response.json()
+        print(data)
+        return data
+    except Exception as e:
+        print(e)
+        return None
+
+convert_currency("CAD")
